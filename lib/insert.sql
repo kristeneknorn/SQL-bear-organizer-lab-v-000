05d2f85d5f2dd4cc192853273f018729f128b1ad
kristeneknorn <kristen.knorn@gmail.com>
2021-05-01 17:43:30 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-sqlite> INSERT INTO bears (id, name, age, gender, color, tempermant,alive) VALUES (1, "Mr. Chocolate", 3 , "Male", "Black", 1);
-sqlite> INSERT INTO bears (id, name, age, gender, color, tempermant,alive) VALUES (2, "Rowdy", 4 , "Male", "Brown", 0);
-sqlite> INSERT INTO bears (id, name, age, gender, color, tempermant,alive) VALUES (3, "Tabitha", 4 , "Female", "Brown", 0);
-sqlite> INSERT INTO bears (id, name, age, gender, color, tempermant,alive) VALUES (4, "Sergeant Brown", 5 , "Female", "Brown", 1);
-sqlite> INSERT INTO bears (id, name, age, gender, color, tempermant,alive) VALUES (5, "Melissa", 5 , "Female", "Brown", 1);
-sqlite> INSERT INTO bears (id, name, age, gender, color, tempermant,alive) VALUES (6, "Grinch", 5 , "Male", "Black", 1);
-sqlite> INSERT INTO bears (id, name, age, gender, color, tempermant,alive) VALUES (7, "Wendy", 5 , "Female", "Black", 0);
-sqlite> INSERT INTO bears (id, name, age, gender, color, tempermant,alive) VALUES (7, NULL, 8, "Female", "Black", 0);
+sqlite> INSERT INTO bears (id, name, age, gender, color, tempermant,alive) VALUES (1, "Mr. Chocolate", 3 , "Male", "Black", "dull", 1);
+sqlite> INSERT INTO bears (id, name, age, gender, color, tempermant,alive) VALUES (2, "Rowdy", 4 , "Male", "Brown", "loud", 0);
+sqlite> INSERT INTO bears (id, name, age, gender, color, tempermant,alive) VALUES (3, "Tabitha", 4 , "Female", "Brown", "tired", 0);
+sqlite> INSERT INTO bears (id, name, age, gender, color, tempermant,alive) VALUES (4, "Sergeant Brown", 5 , "Female", "Brown", "annoyed", 1);
+sqlite> INSERT INTO bears (id, name, age, gender, color, tempermant,alive) VALUES (5, "Melissa", 5 , "Female", "Brown", "done", 1);
+sqlite> INSERT INTO bears (id, name, age, gender, color, tempermant,alive) VALUES (6, "Grinch", 5 , "Male", "Black", "annoyed", 1);
+sqlite> INSERT INTO bears (id, name, age, gender, color, tempermant,alive) VALUES (7, "Wendy", 5 , "Female", "Black", "tired", 0);
+sqlite> INSERT INTO bears (id, name, age, gender, color, tempermant,alive) VALUES (7, NULL, 8, "Female", "Black", "loud", 0);
