1cf2e90445c005fc27f34461b93ec66c4dbe2f54
kristeneknorn <kristen.knorn@gmail.com>
2021-05-01 17:53:13 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-sqlite> INSERT INTO bears (id, name, age, gender, color, tempermant,alive) VALUES ("Mr. Chocolate", 3 , "Male", "Black", "dull", 1);
-sqlite> INSERT INTO bears (id, name, age, gender, color, tempermant,alive) VALUES ("Rowdy", 4 , "Male", "Brown", "loud", 0);
-sqlite> INSERT INTO bears (id, name, age, gender, color, tempermant,alive) VALUES ("Tabitha", 4 , "Female", "Brown", "tired", 0);
-sqlite> INSERT INTO bears (id, name, age, gender, color, tempermant,alive) VALUES ("Sergeant Brown", 5 , "Female", "Brown", "annoyed", 1);
-sqlite> INSERT INTO bears (id, name, age, gender, color, tempermant,alive) VALUES ("Melissa", 5 , "Female", "Brown", "done", 1);
-sqlite> INSERT INTO bears (id, name, age, gender, color, tempermant,alive) VALUES ("Grinch", 5 , "Male", "Black", "annoyed", 1);
-sqlite> INSERT INTO bears (id, name, age, gender, color, tempermant,alive) VALUES ("Wendy", 5 , "Female", "Black", "tired", 0);
-sqlite> INSERT INTO bears (id, name, age, gender, color, tempermant,alive) VALUES (NULL, 8, "Female", "Black", "loud", 0);
+INSERT INTO bears (id, name, age, gender, color, tempermant,alive) VALUES ("Mr. Chocolate", 3 , "Male", "Black", "dull", 1);
+INSERT INTO bears (id, name, age, gender, color, tempermant,alive) VALUES ("Rowdy", 4 , "Male", "Brown", "loud", 0);
+INSERT INTO bears (id, name, age, gender, color, tempermant,alive) VALUES ("Tabitha", 4 , "Female", "Brown", "tired", 0);
+INSERT INTO bears (id, name, age, gender, color, tempermant,alive) VALUES ("Sergeant Brown", 5 , "Female", "Brown", "annoyed", 1);
+INSERT INTO bears (id, name, age, gender, color, tempermant,alive) VALUES ("Melissa", 5 , "Female", "Brown", "done", 1);
+INSERT INTO bears (id, name, age, gender, color, tempermant,alive) VALUES ("Grinch", 5 , "Male", "Black", "annoyed", 1);
+INSERT INTO bears (id, name, age, gender, color, tempermant,alive) VALUES ("Wendy", 5 , "Female", "Black", "tired", 0);
+INSERT INTO bears (id, name, age, gender, color, tempermant,alive) VALUES (NULL, 8, "Female", "Black", "loud", 0);
