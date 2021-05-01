71a8f5bd25fedc25ee60999cc19a7cd50d8b18d0
kristeneknorn <kristen.knorn@gmail.com>
2021-05-01 17:36:08 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,1 @@
-sqlite> INSERT INTO bears (id, name, age, gender, color, tempermant,alive) VALUES (1, "Mr. Chocolate", 3 , "Male", "Black", 1)
+sqlite> INSERT INTO bears (id, name, age, gender, color, tempermant,alive) VALUES (1, "Mr. Chocolate", 3 , "Male", "Black", 1);
