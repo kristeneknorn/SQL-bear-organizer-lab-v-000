6c754f350a1e34aec13ba161db7407d63324e5cd
kristeneknorn <kristen.knorn@gmail.com>
2021-05-01 17:44:36 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,5 +1,5 @@
-sqlite> INSERT INTO bears (id, name, age, gender, color, tempermant,alive) VALUES (1, "Mr. Chocolate", 3 , "Male", "Black", "dull", 1);
-sqlite> INSERT INTO bears (id, name, age, gender, color, tempermant,alive) VALUES (2, "Rowdy", 4 , "Male", "Brown", "loud", 0);
+INSERT INTO bears (id, name, age, gender, color, tempermant,alive) VALUES (1, "Mr. Chocolate", 3 , "Male", "Black", "dull", 1);
+INSERT INTO bears (id, name, age, gender, color, tempermant,alive) VALUES (2, "Rowdy", 4 , "Male", "Brown", "loud", 0);
 sqlite> INSERT INTO bears (id, name, age, gender, color, tempermant,alive) VALUES (3, "Tabitha", 4 , "Female", "Brown", "tired", 0);
 sqlite> INSERT INTO bears (id, name, age, gender, color, tempermant,alive) VALUES (4, "Sergeant Brown", 5 , "Female", "Brown", "annoyed", 1);
 sqlite> INSERT INTO bears (id, name, age, gender, color, tempermant,alive) VALUES (5, "Melissa", 5 , "Female", "Brown", "done", 1);
