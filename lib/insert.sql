a7205282a90056c1edf4db0e9580da6c82c5dc80
kristeneknorn <kristen.knorn@gmail.com>
2021-05-01 17:45:07 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
 INSERT INTO bears (id, name, age, gender, color, tempermant,alive) VALUES (1, "Mr. Chocolate", 3 , "Male", "Black", "dull", 1);
 INSERT INTO bears (id, name, age, gender, color, tempermant,alive) VALUES (2, "Rowdy", 4 , "Male", "Brown", "loud", 0);
-sqlite> INSERT INTO bears (id, name, age, gender, color, tempermant,alive) VALUES (3, "Tabitha", 4 , "Female", "Brown", "tired", 0);
-sqlite> INSERT INTO bears (id, name, age, gender, color, tempermant,alive) VALUES (4, "Sergeant Brown", 5 , "Female", "Brown", "annoyed", 1);
-sqlite> INSERT INTO bears (id, name, age, gender, color, tempermant,alive) VALUES (5, "Melissa", 5 , "Female", "Brown", "done", 1);
-sqlite> INSERT INTO bears (id, name, age, gender, color, tempermant,alive) VALUES (6, "Grinch", 5 , "Male", "Black", "annoyed", 1);
-sqlite> INSERT INTO bears (id, name, age, gender, color, tempermant,alive) VALUES (7, "Wendy", 5 , "Female", "Black", "tired", 0);
-sqlite> INSERT INTO bears (id, name, age, gender, color, tempermant,alive) VALUES (7, NULL, 8, "Female", "Black", "loud", 0);
+INSERT INTO bears (id, name, age, gender, color, tempermant,alive) VALUES (3, "Tabitha", 4 , "Female", "Brown", "tired", 0);
+INSERT INTO bears (id, name, age, gender, color, tempermant,alive) VALUES (4, "Sergeant Brown", 5 , "Female", "Brown", "annoyed", 1);
+INSERT INTO bears (id, name, age, gender, color, tempermant,alive) VALUES (5, "Melissa", 5 , "Female", "Brown", "done", 1);
+INSERT INTO bears (id, name, age, gender, color, tempermant,alive) VALUES (6, "Grinch", 5 , "Male", "Black", "annoyed", 1);
+INSERT INTO bears (id, name, age, gender, color, tempermant,alive) VALUES (7, "Wendy", 5 , "Female", "Black", "tired", 0);
+INSERT INTO bears (id, name, age, gender, color, tempermant,alive) VALUES (7, NULL, 8, "Female", "Black", "loud", 0);
